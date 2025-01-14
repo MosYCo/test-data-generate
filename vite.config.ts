1a7ff57e84489ab419a9068126e133124a510435
yuyang <yuyang@7moor.com>
2025-01-14 15:18:21 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: '/test-data-generator/', // Replace with your repository name
+  base: '/test-data-generate/', // Replace with your repository name
 });
